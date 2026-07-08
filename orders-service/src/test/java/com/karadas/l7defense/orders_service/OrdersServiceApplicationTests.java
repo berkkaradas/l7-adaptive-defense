f7f66de6d4754dcac4b92b74cb2c14416bfb0214
Omer Karadas <2546262@swansea.ac.uk>
2026-07-08 15:41:12 +0100

    Initial project skeleton: parent POM, auth-service and orders-service modules, docker-compose infrastructure

---
+++ orders-service/src/test/java/com/karadas/l7defense/orders_service/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karadas.l7defense.orders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
